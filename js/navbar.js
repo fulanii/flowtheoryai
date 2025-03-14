3b8cd4e9c80ad4203a7208e190dbf6186ed8a888
Yassine Issoufou <68094236+fulanii@users.noreply.github.com>
2025-03-14 06:20:10 -0500

    added services section

--- js/navbar.js
+++ js/navbar.js
@@ -2,9 +2,25 @@
 
 const hambuger = document.querySelector(".bi-list");
 const modal = document.querySelector(".modal");
-
+const bookBtn = document.querySelectorAll(".nav-cta-btn");
 // toggle modal on click
 hambuger.addEventListener("click", (event) => {
   event.preventDefault();
   modal.style.display = "flex";
+});
+
+// close modal on click
+document.querySelectorAll('.modal-item a').forEach(link => {
+  link.addEventListener('click', function () {
+    modal.style.display = "none" // Hide modal
+  });
+});
+
+// go to booking section
+bookBtn.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    event.preventDefault();
+    modal.style.display = "none" // Hide modal
+    document.getElementById("booking").scrollIntoView({ behavior: "smooth" });
+  });
 });
