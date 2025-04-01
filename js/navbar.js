003a622d86a7b739ba299f2d69941c141e58da7d
Yassine Issoufou <68094236+fulanii@users.noreply.github.com>
2025-03-31 22:09:23 -0500

    redisegn part 1 done

--- js/navbar.js
+++ js/navbar.js
@@ -3,6 +3,7 @@
 const hambuger = document.querySelector(".bi-list");
 const modal = document.querySelector(".modal");
 const bookBtn = document.querySelectorAll(".nav-cta-btn");
+
 // toggle modal on click
 hambuger.addEventListener("click", (event) => {
   event.preventDefault();
@@ -15,14 +16,4 @@ document.querySelectorAll('.modal-item a').forEach(link => {
     modal.style.display = "none" // Hide modal
   });
 });
-
-// go to booking section
-bookBtn.forEach((btn) => {
-  btn.addEventListener("click", (event) => {
-    event.preventDefault();
-    modal.style.display = "none" // Hide modal
-    document.getElementById("booking").scrollIntoView({ behavior: "smooth" });
-  });
-});
-
 
