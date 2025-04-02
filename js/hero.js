8bef6856e5b81720b2c01392e755f7b8d0cbf621
Yassine Issoufou <68094236+fulanii@users.noreply.github.com>
2025-04-02 15:45:11 -0500

    added contact section

--- js/hero.js
+++ js/hero.js
@@ -1,8 +1,17 @@
 const exploreBtn = document.querySelector(".explore-btn");
+const journeyBtn = document.querySelector(".journey-btn"); 
+const contactSection = document.querySelector("#contact-section");
 
 exploreBtn.addEventListener("click", (event) => {
   event.preventDefault();
   serviceSection.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
+});
+journeyBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  contactSection.scrollIntoView({
     behavior: "smooth",
     block: "start",
   });
