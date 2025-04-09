b573feab104b9252385ec9e98f7c41f9cf6ab09c
Yassine Issoufou <68094236+fulanii@users.noreply.github.com>
2025-04-08 19:10:58 -0500

    finished form submission

--- js/contact.js
+++ js/contact.js
@@ -6,6 +6,8 @@ const qBookBtn = document.querySelector(".book-btn");
 const infoFormDiv = document.querySelector("#info-form-div");
 const bookDiv = document.querySelector("#book-div");
 
+const form = document.querySelector("#contact-form");
+
 qBookBtn.addEventListener("click", function () {
   quickBtn.classList.remove("btn-active");
   qBookBtn.classList.add("btn-active");
@@ -13,10 +15,7 @@ qBookBtn.addEventListener("click", function () {
   infoFormDiv.style.display = "none";
   bookDiv.style.display = "flex";
   bookDiv.style.minWidth = "90vw";
-  bookDiv.scrollIntoView({ behavior: 'smooth', block: 'center' });
-  
-;
-
+  bookDiv.scrollIntoView({ behavior: "smooth", block: "center" });
 });
 
 quickBtn.addEventListener("click", function () {
@@ -25,5 +24,29 @@ quickBtn.addEventListener("click", function () {
 
   infoFormDiv.style.display = "flex";
   bookDiv.style.display = "none";
-  infoFormDiv.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  infoFormDiv.scrollIntoView({ behavior: "smooth", block: "center" });
+});
+
+// form submission
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData.entries());
+
+  fetch("https://hook.us2.make.com/4q2jpc1iavy48s2vlj75dzepueajq7p3", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response)
+    .then((data) => {
+
+      if (data.status === 200 && data.ok === true) {
+        alert("Form submitted successfully!");
+        form.reset();
+      }
+      
+    });
 });
